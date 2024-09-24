a647bdeb76ca8215b9dcdd29f58e06f34e524a07
projecttag <152490507+projecttag@users.noreply.github.com>
2024-09-24 15:11:44 +0530

    added changes

--- 02_variables-and-numbers/task_03.js
+++ 02_variables-and-numbers/task_03.js
@@ -2,16 +2,16 @@ let n = 0;
 let m = 100;
 
 let range = Math.abs(n - m);
-let numberInRange = Math.round(Math.random() * range);
+let numberInRange = Math.round(Math.random() * range);         // numberInRange помещен в переменную let
+let numberInRangeSecond = Math.round(Math.random() * range);   // numberInRangeSecond помещен в переменную let
 let min = Math.min(n,m);
-let max = Math.max(n,m);
 
-// console.log(min + numberInRange);
-// console.log(max + numberInRange);
+console.log(min + numberInRange);       // вывод в консоль, первое число
+console.log(min + numberInRangeSecond); // вывод в консоль, второе число
 
-console.log('Первое число больше', max > min);
-console.log('Первое число больше', max < min);
-console.log('Первое число больше', max >= min);
-console.log('Первое число больше', max <= min);
-console.log('Первое число больше', max === min);
-console.log('Первое число больше', max !== min);
+console.log('Первое число больше второго?', numberInRange > numberInRangeSecond); // исправлено, тк сравниваем числа друг с другом а не min и min
+console.log('Первое число меньше второго?', numberInRange < numberInRangeSecond);
+console.log('Первое число больше или равно второму?', numberInRange >= numberInRangeSecond);
+console.log('Первое число меньше или равно второму?', numberInRange <= numberInRangeSecond);
+console.log('Первое число равно второму?', numberInRange === numberInRangeSecond);
+console.log('Первое число не равно второму?', numberInRange !== numberInRangeSecond);
