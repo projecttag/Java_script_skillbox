39cc14016abf7776cf281c941a134718818988c8
projecttag <152490507+projecttag@users.noreply.github.com>
2024-09-25 21:59:41 +0530

    added changes

--- 02_variables-and-numbers/task_03.js
+++ 02_variables-and-numbers/task_03.js
@@ -2,16 +2,20 @@ let n = 0;
 let m = 100;
 
 let range = Math.abs(n - m);
-let numberInRange = Math.round(Math.random() * range);         // numberInRange помещен в переменную let
-let numberInRangeSecond = Math.round(Math.random() * range);   // numberInRangeSecond помещен в переменную let
-let min = Math.min(n,m);
+let numberInRange = Math.round(Math.random() * range);         // первое случайное число
+let numberInRangeSecond = Math.round(Math.random() * range);   // второе случайное число
+let min = Math.min(n, m);
 
-console.log(min + numberInRange);       // вывод в консоль, первое число
-console.log(min + numberInRangeSecond); // вывод в консоль, второе число
+let firstNumber = min + numberInRange;      // итоговое первое число
+let secondNumber = min + numberInRangeSecond; // итоговое второе число
 
-console.log('Первое число больше второго?', numberInRange > numberInRangeSecond); // исправлено, тк сравниваем числа друг с другом а не min и min
-console.log('Первое число меньше второго?', numberInRange < numberInRangeSecond);
-console.log('Первое число больше или равно второму?', numberInRange >= numberInRangeSecond);
-console.log('Первое число меньше или равно второму?', numberInRange <= numberInRangeSecond);
-console.log('Первое число равно второму?', numberInRange === numberInRangeSecond);
-console.log('Первое число не равно второму?', numberInRange !== numberInRangeSecond);
+console.log(firstNumber);  // вывод в консоль, первое число
+console.log(secondNumber); // вывод в консоль, второе число
+
+// Сравнения двух получившихся чисел
+console.log('Первое число больше второго?', firstNumber > secondNumber);
+console.log('Первое число меньше второго?', firstNumber < secondNumber);
+console.log('Первое число больше или равно второму?', firstNumber >= secondNumber);
+console.log('Первое число меньше или равно второму?', firstNumber <= secondNumber);
+console.log('Первое число равно второму?', firstNumber === secondNumber);
+console.log('Первое число не равно второму?', firstNumber !== secondNumber);
