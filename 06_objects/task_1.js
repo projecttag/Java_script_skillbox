1b3677dc39cf774157d6ef0f88f7ac710e2f8275
projecttag <152490507+projecttag@users.noreply.github.com>
2024-10-08 11:17:06 +0530

    home work done

--- 06_objects/task_1.js
+++ 06_objects/task_1.js
@@ -1,4 +1,6 @@
 // Обязательная часть задания
+// В этом задании использовали тернарный оператор :  условие ? выражение1 : выражение2;
+
 let user1={
   name: 'Игорь',
   age: 17
@@ -10,40 +12,77 @@ let user2={
 }
 
 function getOlderUser(userOne, userTwo){
-  // Здесь решение задачи
+  return userOne.age > userTwo.age ? userOne.name : userTwo.name; // команда return используется перед тернарным оператором
 }
 
 let result = getOlderUser(user1, user2)
 console.log('Старший пользователь:',result);
 
+/*
+
+// без тернароного оператора можно решить так:
+
+let user1={
+  name: 'Игорь',
+  age: 17
+}
+
+let user2={
+  name: 'Оля',
+  age: 21
+}
+
+function getOlderUser(userOne, userTwo) {
+  if (userOne.age > userTwo.age) {
+    return userOne.name;
+  } else {
+    return userTwo.name; // команда ретерн используется после оператора иф
+  }
+}
+
+let result = getOlderUser(user1, user2)
+console.log('Старший пользователь:',result);
+
+*/
 
 // // Не обязательная часть задания
-// let allUsers=[
-//   {
-//     name: 'Валя',
-//     age: 11
-//   },
-//   {
-//     name: 'Таня',
-//     age: 24
-//   },
-//   {
-//     name: 'Рома',
-//     age: 21
-//   },
-//   {
-//     name: 'Надя',
-//     age: 34
-//   },
-//   {
-//     name: 'Антон',
-//     age: 7
-//   }
-// ]
 
-// function getOlderUserArray(usersArray){
-//   // Здесь решение задачи
-// }
 
-// let result2 = getOlderUserArray(allUsers)
-// console.log('Старший пользователь:',result2);
+
+let allUsers = [
+  {
+    name: 'Валя',
+    age: 11
+  },
+  {
+    name: 'Таня',
+    age: 24
+  },
+  {
+    name: 'Рома',
+    age: 21
+  },
+  {
+    name: 'Надя',
+    age: 34
+  },
+  {
+    name: 'Антон',
+    age: 7
+  }
+];
+
+function getOlderUserArray(usersArray) {
+  let oldestUser = usersArray[0]; // Начинаем с первого пользователя
+
+  for (let i = 1; i < usersArray.length; i++) {
+    if (usersArray[i].age > oldestUser.age) {
+      oldestUser = usersArray[i]; // Если найден пользователь старше, обновляем переменную
+    }
+  }
+
+  return oldestUser.name; // Возвращаем имя самого старшего пользователя
+}
+
+let result2 = getOlderUserArray(allUsers);
+console.log('Старший пользователь:', result2);
--- 06_objects/task_3.js
+++ 06_objects/task_3.js
@@ -2,14 +2,13 @@ let objects = [
   { name: 'Василий', surname: 'Васильев' },
   { name: 'Иван', surname: 'Иванов' },
   { name: 'Пётр', surname: 'Петров' }
-]
- 
+];
 
-function filter(arr, prop, value){
-  // Здесь решение задачи
-  // arr - массив объектов
-  // prop - свойство по которому производится фильтрация
-  // value - значение свойства по которому производится фильтрация
+
+function filter(arr, prop, value) {
+  return arr.filter(function(obj) {
+    return obj[prop] === value;
+  });
 }
 
 let result = filter(objects, 'name', 'Иван');
